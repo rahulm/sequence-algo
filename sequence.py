cacea5173c44dfc11bd6a98577cb7e752dc77763
Rahul Malavalli <rahul.malavalli@gmail.com>
2019-11-22 17:20:44 -0800

    added first python file with argparse skeleton

---
+++ sequence.py
@@ -0,0 +1,26 @@
+import argparse
+
+def main(args):
+  print(args)
+
+
+def parseArgs():
+  parser = argparse.ArgumentParser(description = "Taking a shot at solving Sequence.")
+  parser.add_argument(
+    "--players", type = int, required = True,
+    choices = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+    help = "Total number of players."
+  )
+  parser.add_argument(
+    "--teams", type = int, required = True,
+    help = "Number of teams. If the number of teams equals the number of players, each player is individual. Players must be divisible by teams."
+  )
+  
+  args = parser.parse_args()
+  if ((args.players % args.teams) != 0):
+    parser.error("--players ({}) is not divisble by --teams ({}).".format(args.players, args.teams))
+  
+  return args
+
+if __name__ == "__main__":
+  main(parseArgs())
