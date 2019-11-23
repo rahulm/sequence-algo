8540afeb476354f2fcca940fbf7c880544967d29
Rahul Malavalli <rahul.malavalli@gmail.com>
2019-11-22 17:34:20 -0800

    fixed number of teams

--- sequence.py
+++ sequence.py
@@ -13,14 +13,13 @@ def parseArgs():
   )
   parser.add_argument(
     "--teams", type = int, required = True,
+    choices = {2, 3},
     help = "Number of teams. If the number of teams equals the number of players, each player is individual. Players must be divisible by teams."
   )
   
   args = parser.parse_args()
   if ((args.players % args.teams) != 0):
     parser.error("--players ({}) is not divisble by --teams ({}).".format(args.players, args.teams))
-  if ((args.players > 3) and (args.teams == args.players)):
-    parser.error("Only 3 players may play individually; the {} players provided must be split into teams.".format(args.players))
   
   return args
 
