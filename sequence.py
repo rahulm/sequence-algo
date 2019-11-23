5034bbd4e06c19c71b3a58a5fd38107d837838c6
Rahul Malavalli <rahul.malavalli@gmail.com>
2019-11-22 19:55:20 -0800

    added num cards per hand and sequences to win

--- sequence.py
+++ sequence.py
@@ -1,7 +1,27 @@
 import argparse
 
+CARDS_PER_HAND_PER_PLAYER_COUNT = {
+  2 : 7, 3 : 6, 4 : 6, 6 : 5, 8 : 4, 9 : 4, 10 : 3, 12 : 3
+}
+
+SEQUENCES_PER_TEAM_COUNT = {
+  2 : 2, 3 : 1
+}
+
 def main(args):
+  print("Sequence Algorithm")
+  print("- Blind 'n Greedy (BNG) Version -\n")
   print(args)
+  
+  numPlayers = args.players
+  numTeams = args.teams
+  numCardsPerHand = CARDS_PER_HAND_PER_PLAYER_COUNT[numPlayers]
+  numSequencesToWin = SEQUENCES_PER_TEAM_COUNT[numTeams]
+  
+  print("Number of cards per hand: {}".format(numCardsPerHand))
+  print("Number of sequences to win: {}".format(numSequencesToWin))
+  
+  
 
 
 def parseArgs():
